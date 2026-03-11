2f759dd2cecb18ff4c96af7d444afdfd14fb249d
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-03-11 11:45:47 +0530

    fix swiper SSR issue

--- src/components/ClinicalFocusSlider.js
+++ src/components/ClinicalFocusSlider.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination } from "swiper/modules"
 import { useStaticQuery, graphql, Link } from "gatsby"
@@ -7,6 +7,12 @@ import "swiper/css"
 import "swiper/css/pagination"
 
 const ClinicalFocusSlider = ({ pageData }) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   const data = useStaticQuery(graphql`
     query ExpertiseQuery {
       allWpExpertise {
@@ -45,55 +51,40 @@ const ClinicalFocusSlider = ({ pageData }) => {
           dangerouslySetInnerHTML={{ __html: pageData?.clinicalFocusPara }}
         />
 
-        <Swiper
-          className="clinical-swiper"
-          modules={[Pagination]}
-          pagination={{ clickable: true }}
-          grabCursor={true}
-          slidesPerView={4}
-          slidesPerGroup={4}
-          spaceBetween={30}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            350: {
-              slidesPerView: 2,
-              slidesPerGroup: 2,
-            },
-            600: {
-              slidesPerView: 3,
-              slidesPerGroup: 3,
-            },
-            1024: {
-              slidesPerView: 4,
-              slidesPerGroup: 4,
-            },
-            1280: {
-              slidesPerView: 4,
-              slidesPerGroup: 4,
-              spaceBetween: 50,
-            },
-          }}
-        >
-          {slides.map(({ node }, index) => (
-            <SwiperSlide key={index}>
-              <Link to={`/expertise#${node.slug}`} className="clinical-card">
-                <div className="card-img">
-                  {node.featuredImage?.node?.mediaItemUrl && (
-                    <img
-                      src={node.featuredImage.node.mediaItemUrl}
-                      alt={node.featuredImage.node.altText || node.title}
-                    />
-                  )}
-                </div>
-
-                <h4>{node.title}</h4>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {isClient && (
+          <Swiper
+            className="clinical-swiper"
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+            grabCursor={true}
+            slidesPerView={4}
+            slidesPerGroup={4}
+            spaceBetween={30}
+            breakpoints={{
+              0: { slidesPerView: 1, slidesPerGroup: 1 },
+              350: { slidesPerView: 2, slidesPerGroup: 2 },
+              600: { slidesPerView: 3, slidesPerGroup: 3 },
+              1024: { slidesPerView: 4, slidesPerGroup: 4 },
+              1280: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 50 },
+            }}
+          >
+            {slides.map(({ node }, index) => (
+              <SwiperSlide key={index}>
+                <Link to={`/expertise#${node.slug}`} className="clinical-card">
+                  <div className="card-img">
+                    {node.featuredImage?.node?.mediaItemUrl && (
+                      <img
+                        src={node.featuredImage.node.mediaItemUrl}
+                        alt={node.featuredImage.node.altText || node.title}
+                      />
+                    )}
+                  </div>
+                  <h4>{node.title}</h4>
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </section>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
 import ClinicalFocusSlider from "../components/ClinicalFocusSlider"
 import AppointmentForm from "../components/AppointmentForm";
 
