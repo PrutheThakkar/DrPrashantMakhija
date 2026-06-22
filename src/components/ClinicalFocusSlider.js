5e94f974adf1dcf6563ccee3efb249ca4ff93c74
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-06-22 15:59:04 +0530

    update fix Monday

--- src/components/ClinicalFocusSlider.js
+++ src/components/ClinicalFocusSlider.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination, Navigation } from "swiper/modules"
 import { useStaticQuery, graphql, Link } from "gatsby"
-import AOS from "aos"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import "swiper/css"
 import "swiper/css/pagination"
@@ -16,11 +16,15 @@ const ClinicalFocusSlider = ({ pageData }) => {
   }, [])
 
   useEffect(() => {
-    if (isClient) {
-      setTimeout(() => {
-        AOS.refreshHard()
-      }, 300)
-    }
+    if (!isClient) return
+
+    const timer = setTimeout(() => {
+      if (typeof window !== "undefined" && window.AOS) {
+        window.AOS.refresh()
+      }
+    }, 300)
+
+    return () => clearTimeout(timer)
   }, [isClient])
 
   const data = useStaticQuery(graphql`
@@ -31,13 +35,18 @@ const ClinicalFocusSlider = ({ pageData }) => {
             slug
             title
             uri
-            content
             featuredImage {
               node {
                 altText
-                mediaItemUrl
                 title
-                slug
+                gatsbyImage(
+                  width: 520
+                  height: 420
+                  quality: 75
+                  layout: CONSTRAINED
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
               }
             }
           }
@@ -46,7 +55,7 @@ const ClinicalFocusSlider = ({ pageData }) => {
     }
   `)
 
-  const slides = data.allWpExpertise.edges.slice().reverse()
+  const slides = data?.allWpExpertise?.edges?.slice().reverse() || []
 
   return (
     <section
@@ -83,44 +92,71 @@ const ClinicalFocusSlider = ({ pageData }) => {
               }}
               grabCursor={true}
               slidesPerView={4}
-              speed={1500}
+              speed={900}
               slidesPerGroup={4}
               spaceBetween={30}
               breakpoints={{
-                0: { slidesPerView: 1, slidesPerGroup: 1 },
-                350: { slidesPerView: 1, slidesPerGroup: 1 },
-                720: { slidesPerView: 3, slidesPerGroup: 2 },
-                1024: { slidesPerView: 4, slidesPerGroup: 4 },
-                1280: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 50 },
+                0: {
+                  slidesPerView: 1,
+                  slidesPerGroup: 1,
+                  spaceBetween: 18,
+                },
+                720: {
+                  slidesPerView: 2,
+                  slidesPerGroup: 1,
+                  spaceBetween: 24,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  slidesPerGroup: 4,
+                  spaceBetween: 30,
+                },
+                1280: {
+                  slidesPerView: 4,
+                  slidesPerGroup: 4,
+                  spaceBetween: 50,
+                },
               }}
               onInit={() => {
                 setTimeout(() => {
-                  AOS.refreshHard()
+                  if (typeof window !== "undefined" && window.AOS) {
+                    window.AOS.refresh()
+                  }
                 }, 300)
               }}
             >
-              {slides.map(({ node }, index) => (
-                <SwiperSlide key={index}>
-                  <Link
-                    to={`/expertise#${node.slug}`}
-                    className="clinical-card"
-                    data-aos="fade-up"
-                    data-aos-delay={(index % 4) * 150}
-                  >
-                    <div className="card-img">
-                      {node.featuredImage?.node?.mediaItemUrl && (
-                        <img
-                          src={node.featuredImage.node.mediaItemUrl}
-                          alt={node.featuredImage.node.altText || node.title}
-                          loading="lazy"
-                        />
-                      )}
-                    </div>
+              {slides.map(({ node }, index) => {
+                const image = getImage(node?.featuredImage?.node?.gatsbyImage)
+                const imageAlt =
+                  node?.featuredImage?.node?.altText ||
+                  node?.featuredImage?.node?.title ||
+                  node?.title ||
+                  "Clinical focus image"
 
-                    <h4 dangerouslySetInnerHTML={{ __html: node.title }} />
-                  </Link>
-                </SwiperSlide>
-              ))}
+                return (
+                  <SwiperSlide key={node.slug || index}>
+                    <Link
+                      to={`/expertise/#${node.slug}`}
+                      className="clinical-card"
+                      data-aos="fade-up"
+                      data-aos-delay={(index % 4) * 100}
+                    >
+                      <div className="card-img">
+                        {image && (
+                          <GatsbyImage
+                            image={image}
+                            alt={imageAlt}
+                            loading={index < 4 ? "eager" : "lazy"}
+                            decoding="async"
+                          />
+                        )}
+                      </div>
+
+                      <h4 dangerouslySetInnerHTML={{ __html: node.title }} />
+                    </Link>
+                  </SwiperSlide>
+                )
+              })}
             </Swiper>
 
             <button className="clinical-prev" aria-label="Previous slide">
